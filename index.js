0f8694f665e41779ca11ac26c506e4339b4a2284
Nestor <nestor.venegas95@gmail.com>
2021-04-08 23:20:22 -0500

    commit tree 6eed51b4ba626e19b1463e7137571b12a4fcdeeb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    //make new driver so we dont update the original driver.
+    // const newDriver =  {...driver}
+    // newDriver[key] = value
+    //return newDriver
+    return Object.assign({},driver,{[key]:value})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    //modify original driver and return it
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    //delete key from a copy of driver and return new driver
+    const newDriver = {...driver} 
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    // delete key from original driver and return driver
+    delete driver[key]
+    return driver
+}
+
+
